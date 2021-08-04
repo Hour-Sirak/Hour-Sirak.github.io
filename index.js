96278c8560255b4b35d38963960fd2369c08b682
Hour-Sirak <hoursirak0102@gmail.com>
2021-08-04 17:16:11 +0700

    Using Canvas

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const randColor = () =>{
 	return "#"+Math.random().toString(16).slice(2, 8)
 }
-let words = ["Cat", "Python", "Javascript", "Ruby", "Haskel", "Dog"]
+let words = ["Google", "Astronomy", "Naruto", "Psychology", "Bek Sloy", "Uwu"]//, "a", "b", "c", "d"]//['안녕하세요!', '소년', '어머니', '바보']
 const canvas = document.getElementById('wheel');
 const ctx = canvas.getContext('2d');
 const w = canvas.width = 400;
@@ -13,22 +13,57 @@ const renderWheel = ()=>{
 	let cy = h/2
 	let radius = 195
 	let anglePerSlice = Math.PI*2 / words.length
-	let startAngle = Math.PI*2 - anglePerSlice
+	let startAngle = Math.PI*3 / 2 + anglePerSlice / 2//Math.PI*2 - anglePerSlice
 	let rev = words.slice().reverse()
+		{// {ctx.restore()
+		// ctx.save()
+		// let endAngle = startAngle + anglePerSlice
+		
+		// ctx.fillStyle = randColor()
+		// ctx.lineWidth = 10
+		// ctx.beginPath()
+		// ctx.lineTo(cx, cy )
+		// ctx.arc(cx, cy, radius, startAngle, endAngle, false)
+		// ctx.stroke()
+		// ctx.fill()
+		// ctx.closePath()
+		
+		// ctx.beginPath()
+		// ctx.font = '18px Helvetica'
+		// ctx.textAlign = 'center'
+		// ctx.fillStyle = "white"
+		// let theta = (startAngle + endAngle) / 2
+		// let tx = cx + Math.cos(theta)*radius/1.9
+		// let ty = cy + Math.sin(theta)*radius/1.9
+		// ctx.translate(tx, ty)
+		// ctx.rotate(theta)
+		// ctx.fillText(word, 0, 5) // 5 for teh text to center
+		// ctx.closePath()
+
+		// console.log(word)
+		// startAngle = endAngle}\
+	// ctx.moveTo(100, 0)
+	// ctx.lineStyle = 'white'
+	// ctx.lineWidth = 10
+	// ctx.lineTo(cx, cy)
+	// ctx.stroke()
+		}
 	rev.forEach(word =>{
 		ctx.restore()
 		ctx.save()
-		let slicePath = new Path2D()
-		ctx.arc(cx, cy, radius, startAngle, endAngle, false)
-
-		ctx.fillStyle = randColor()
-		ctx.lineWidth = 0.9
-		ctx.beginPath()
-		ctx.strokeStyle = 'black'
 		let endAngle = startAngle + anglePerSlice
+		
+		ctx.beginPath()
+		ctx.fillStyle = randColor()
+		ctx.lineWidth = 5
+		ctx.strokeStyle = 'white'
+		// let pos = ctx.getBoundingClientRect
+
 		ctx.lineTo(cx, cy)
-		ctx.fill()
+		ctx.arc(cx, cy, radius, startAngle, endAngle, false)
+		ctx.lineTo(cx, cy)
 		ctx.stroke()
+		ctx.fill()
 		ctx.closePath()
 		
 		ctx.beginPath()
@@ -53,15 +88,10 @@ window.onload = () =>{
 	let degree = 1500
 	let id;
 	spin.onclick = () => {
-		let delay = parseInt(window.getComputedStyle(canvas).transitionDuration.replace('s', ''))
+		let delay = parseInt(window.getComputedStyle(canvas).transitionDuration.replace('s', ''))*1000
 		console.log(delay*1000)
 		clearTimeout(id)
 		degree += Math.floor((Math.random() + 1) * 1500)
-		// canvas.classList.add('spin')
-		// setTimeout(() => {
-		// 	canvas.classList.remove('spin')
-		// }, 2000)
-		// degree = 360
 
 		canvas.style.transform = `rotate(${degree}deg)`
 		let anglePerSliceDeg = 360 / words.length
@@ -71,7 +101,12 @@ window.onload = () =>{
 		id = setTimeout(()=>{
 			let synth = window.speechSynthesis
 			let voices = synth.getVoices()
+			for(var i = 0; i < voices.length; i++) {
+				var option = voices[i].name + ' (' + voices[i].lang + ')';
+				console.log(option)
+			}
 			let msg = new SpeechSynthesisUtterance(words[index])
+			msg.lang = 'ko-KR'
 			speechSynthesis.speak(msg)
 			// msg.voice = voices[]
 		}, delay)
@@ -82,9 +117,11 @@ window.onload = () =>{
 		let word = addText.value
 		if(e.key === 'Enter'){
 			let msg = new SpeechSynthesisUtterance(word)
-			speechSynthesis.speak(msg)	
+			speechSynthesis.speak(msg)
+			addText.value = ''
 			words.push(word)
 			renderWheel()
+
 		}
 	}
 }
