724879dd65c8360f9f22f1ec4826c1b1471a65d8
Hour-Sirak <hoursirak0102@gmail.com>
2021-09-02 21:04:28 +0700

    test speak error 4

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ const koreanLds = document.getElementById("koreanLds");
 let globalWord;
 let source;
 
+let _speechSynth;
+let _voices;
+const _cache = {};
+
 const language = {
     english: "en",
     korean: "ko",
@@ -815,21 +819,93 @@ const toggleDictWidth = () => {
     }
 };
 
-const speak = (word, lang) => {
-    let synth = window.speechSynthesis;
+// const speak = (word, lang) => {
+//     let synth = window.speechSynthesis;
 
-    let voices = synth.getVoices();
+//     let voices = synth.getVoices();
 
-    for (var i = 0; i < voices.length; i++) {
-        console.log(voices[i]);
-        // if (voices[i].default) {
-        // }
+//     for (var i = 0; i < voices.length; i++) {
+//         console.log(voices[i]);
+//         // if (voices[i].default) {
+//         // }
+//     }
+//     try {
+//         let msg = new SpeechSynthesisUtterance(word);
+//         msg.lang = lang;
+//         speechSynthesis.speak(msg);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
+
+/**
+ * retries until there have been voices loaded. No stopper flag included in this example.
+ * Note that this function assumes, that there are voices installed on the host system.
+ */
+
+function loadVoicesWhenAvailable(onComplete = () => {}) {
+    _speechSynth = window.speechSynthesis;
+    const voices = _speechSynth.getVoices();
+
+    if (voices.length !== 0) {
+        _voices = voices;
+        onComplete();
+    } else {
+        return setTimeout(function () {
+            loadVoicesWhenAvailable(onComplete);
+        }, 100);
     }
-    try {
-        let msg = new SpeechSynthesisUtterance(word);
-        msg.lang = lang;
-        speechSynthesis.speak(msg);
-    } catch (error) {
-        console.log(error);
+}
+
+/**
+ * Returns the first found voice for a given language code.
+ */
+
+function getVoices(locale) {
+    if (!_speechSynth) {
+        throw new Error("Browser does not support speech synthesis");
     }
-};
+    if (_cache[locale]) return _cache[locale];
+
+    _cache[locale] = _voices.filter((voice) => voice.lang === locale);
+    return _cache[locale];
+}
+
+/**
+ * Speak a certain text
+ * @param locale the locale this voice requires
+ * @param text the text to speak
+ * @param onEnd callback if tts is finished
+ */
+
+function playByText(locale, text, onEnd) {
+    const voices = getVoices(locale);
+
+    // TODO load preference here, e.g. male / female etc.
+    // TODO but for now we just use the first occurrence
+    const utterance = new window.SpeechSynthesisUtterance();
+    utterance.voice = voices[0];
+    utterance.pitch = 1;
+    utterance.rate = 1;
+    utterance.volume = 1;
+    utterance.rate = 1;
+    utterance.pitch = 0.8;
+    utterance.text = text;
+    utterance.lang = locale;
+
+    if (onEnd) {
+        utterance.onend = onEnd;
+    }
+
+    _speechSynth.cancel(); // cancel current speak, if any is running
+    _speechSynth.speak(utterance);
+}
+
+// on document ready
+loadVoicesWhenAvailable(function () {
+    console.log("loaded");
+});
+
+function speak(word, lang) {
+    playByText(lang, word)
+}
